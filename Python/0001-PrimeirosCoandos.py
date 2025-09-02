0e068712d5fce84b99cd9d7e8ad9f5b7491e227d
Kaauthama <kaauthama@gmail.com>
2025-09-02 18:03:35 -0300

    aaa

---
+++ Python/0001-PrimeirosCoandos.py
@@ -0,0 +1,31 @@
+#pegando nome
+nome = input("Qual o seu nome? ")
+print("Olá" , nome , " Prazer em te conhecer")
+
+# pegando idade
+idade = input("Qual a seu idade? ")
+print("Show ",idade, "! qual o mês de nascimento?")
+
+# pegando ano
+ano = input("Qual o ano do seu nascimento?")
+print(ano,"Ano irado")
+
+# pegando mes
+mes = input("Qual o seu mês de nascimento?")
+print("mes das férias, irado")
+
+# pegando mes dia
+dia = input ("Qual o dia do seu nascimento?")
+print("dia irado !")
+
+
+# fechamento
+print("Prazer conhecer você",nome,", nascida em" ,dia ,"de" ,mes ,"de", ano ," Correto?")
+resposta = input ("sim ou não?")
+
+
+# # dia = 16
+# # mes = 7
+# # ano = 2025
+# # # print (Dia,Mes,Ano)
+# # # print("Você nasceu no dia" ,Dia , Mes , Ano ," Correto?")
