610c2628b42cb1ab249bda8878aef65209a29672
Kaauthama <kaauthama@gmail.com>
2025-09-15 15:28:52 -0300

    organizando

---
+++ Python/Aula 7.py
@@ -0,0 +1,26 @@
+# Soma
+# n1=int(input("Digite um número:"))
+# n2=int(input("digite outro numero:"))
+# s=n1+n2
+# print("A soma entre",n1,"e",n2,"vale", s )
+# print ("A soma entre {} e {} vale {}".format(n1,n2,s))
+
+# # Subtração
+# n1=int(input("Digite um número:"))
+# n2=int(input("digite outro numero:"))
+# s=n1- n2
+# print("A subtração entre",n1,"e",n2,"vale", s )
+# print ("A subtração entre {} e {} vale {}".format(n1,n2,s))
+
+#Multiplicação
+# s=int(input("digite um número:"))
+# d=int(input("Digite outro número:"))
+# z=s*d
+# print (" A multiplicação entre {} e {} vale {}".format(s,d,z))
+
+#Divisão
+# s=int(input("digite um número:"))
+# d=int(input("Digite outro número:"))
+# z=s/d
+# print (" A divisão entre {} e {} vale {}".format(s,d,z))
+
---
+++ Python/exercico 019.py
@@ -0,0 +1,16 @@
+#019
+# import random
+# n1 = str(input("Primeiro Aluno: "))
+# n2 = str(input("Segundo Aluno: "))
+# n3 = str(input("Terceiro Aluno: "))
+# lista = [n1,n2,n3]
+# escolhido= random.choice(lista)
+# print("O Aluno escolhido foi {}".format(escolhido))
+# # fron radom import choice
+
+# Desafio020
+# n1 = input(("One Student: "))
+# n2 = input("Two Student: ")
+# n3 = input("Three Student")
+# lista = [n1,n2,n3]
+# PRINT("a ORDEM ESCOLHIDA FOI{}".format.(shuffle))
